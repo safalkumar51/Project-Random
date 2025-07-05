5b6bd841c1185722f783fe8946d5f0b4dd1ac921
Ashutosh100404 <divusingh1004@gmail.com>
2025-07-05 14:11:42 +0530

    like and comment

---
+++ backend/models/like.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const likeSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Like', likeSchema);
